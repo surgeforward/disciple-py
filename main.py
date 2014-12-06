aa293d5b3c3b22d4e1bb2d4ab8bb7f0098093bc5
Bryan Joseph <bryanjos@gmail.com>
2014-12-06 16:58:23 -0600

    Added sample test. Added disciple folder

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     mongoengine.connect(config.MONGO_DATABASE, host=config.MONGO_HOST,
                         port=config.MONGO_PORT, username=config.MONGO_USERNAME,
                         password=config.MONGO_PASSWORD)
-    print 'hello world'
+    print('hello world')
 
 if __name__ == '__main__':
     main()
---
+++ tests/test_disciple.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestDisciple(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
